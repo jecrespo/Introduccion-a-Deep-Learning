362b8b40c8c5159e9d39e4b65ff3dba270161e44
Enrique Crespo <jecrespom@gmail.com>
2019-12-12 20:53:53 +0100

    Dia7

---
+++ Dia7/dcgan.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Reshape, Dense, Dropout, Flatten
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Conv2D, UpSampling2D
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import backend as K
+from tensorflow.keras import initializers
+
+
+# In[4]:
+
+
+#K.set_image_dim_ordering('th')
+K.set_image_data_format('channels_first')
+
+# Deterministic output.
+# Tired of seeing the same results every time? Remove the line below.
+np.random.seed(1000)
+
+# The results are a little better when the dimensionality of the random vector is only 10.
+# The dimensionality has been left at 100 for consistency with other GAN implementations.
+randomDim = 100
+
+# Load MNIST data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = (X_train.astype(np.float32) - 127.5)/127.5
+X_train = X_train[:, np.newaxis, :, :]
+
+# Optimizer
+adam = Adam(lr=0.0002, beta_1=0.5)
+
+
+# In[5]:
+
+
+# Generator
+generator = Sequential()
+generator.add(Dense(128*7*7, input_dim=randomDim, kernel_initializer=initializers.RandomNormal(stddev=0.02)))
+generator.add(LeakyReLU(0.2))
+generator.add(Reshape((128, 7, 7)))
+generator.add(UpSampling2D(size=(2, 2)))
+generator.add(Conv2D(64, kernel_size=(5, 5), padding='same'))
+generator.add(LeakyReLU(0.2))
+generator.add(UpSampling2D(size=(2, 2)))
+generator.add(Conv2D(1, kernel_size=(5, 5), padding='same', activation='tanh'))
+generator.compile(loss='binary_crossentropy', optimizer=adam)
+
+
+# In[6]:
+
+
+# Discriminator
+discriminator = Sequential()
+discriminator.add(Conv2D(64, kernel_size=(5, 5), strides=(2, 2), padding='same', input_shape=(1, 28, 28), kernel_initializer=initializers.RandomNormal(stddev=0.02)))
+discriminator.add(LeakyReLU(0.2))
+discriminator.add(Dropout(0.3))
+discriminator.add(Conv2D(128, kernel_size=(5, 5), strides=(2, 2), padding='same'))
+discriminator.add(LeakyReLU(0.2))
+discriminator.add(Dropout(0.3))
+discriminator.add(Flatten())
+discriminator.add(Dense(1, activation='sigmoid'))
+discriminator.compile(loss='binary_crossentropy', optimizer=adam)
+
+
+# In[7]:
+
+
+# Combined network
+discriminator.trainable = False
+ganInput = Input(shape=(randomDim,))
+x = generator(ganInput)
+
+
+# In[8]:
+
+
+x
+
+
+# In[9]:
+
+
+ganOutput = discriminator(x)
+gan = Model(inputs=ganInput, outputs=ganOutput)
+gan.compile(loss='binary_crossentropy', optimizer=adam)
+
+dLosses = []
+gLosses = []
+
+
+# In[10]:
+
+
+# Plot the loss from each batch
+def plotLoss(epoch):
+    plt.figure(figsize=(10, 8))
+    plt.plot(dLosses, label='Discriminitive loss')
+    plt.plot(gLosses, label='Generative loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('images/dcgan_loss_epoch_%d.png' % epoch)
+
+# Create a wall of generated MNIST images
+def plotGeneratedImages(epoch, examples=100, dim=(10, 10), figsize=(10, 10)):
+    noise = np.random.normal(0, 1, size=[examples, randomDim])
+    generatedImages = generator.predict(noise)
+
+    plt.figure(figsize=figsize)
+    for i in range(generatedImages.shape[0]):
+        plt.subplot(dim[0], dim[1], i+1)
+        plt.imshow(generatedImages[i, 0], interpolation='nearest', cmap='gray_r')
+        plt.axis('off')
+    plt.tight_layout()
+    plt.savefig('images/dcgan_generated_image_epoch_%d.png' % epoch)
+
+# Save the generator and discriminator networks (and weights) for later use
+def saveModels(epoch):
+    generator.save('models/dcgan_generator_epoch_%d.h5' % epoch) 
+    discriminator.save('models/dcgan_discriminator_epoch_%d.h5' % epoch)
+
+
+# In[11]:
+
+
+def train(epochs=1, batchSize=128):
+    batchCount = X_train.shape[0] / batchSize
+    print ('Epochs:', epochs)
+    print ('Batch size:', batchSize)
+    print ('Batches per epoch:', batchCount)
+
+    for e in range(1, epochs+1):
+        print ('-'*15, 'Epoch %d' % e, '-'*15)
+        for _ in tqdm(range(batchCount)):  #ruido aleatorio
+            # Get a random set of input noise and images
+            noise = np.random.normal(0, 1, size=[batchSize, randomDim])
+            imageBatch = X_train[np.random.randint(0, X_train.shape[0], size=batchSize)]
+
+            # Generate fake MNIST images
+            generatedImages = generator.predict(noise)
+            X = np.concatenate([imageBatch, generatedImages])
+
+            # Labels for generated and real data
+            yDis = np.zeros(2*batchSize)
+            # One-sided label smoothing
+            yDis[:batchSize] = 0.9 #imagenes reales
+
+            # Train discriminator
+            discriminator.trainable = True #solo entreno el discriminador
+            dloss = discriminator.train_on_batch(X, yDis)
+
+            # Train generator
+            noise = np.random.normal(0, 1, size=[batchSize, randomDim])
+            yGen = np.ones(batchSize)
+            discriminator.trainable = False #no entreno el genrador
+            gloss = gan.train_on_batch(noise, yGen) #entreno el conjunto, toda la GAN
+
+        # Store loss of most recent batch from this epoch
+        dLosses.append(dloss)
+        gLosses.append(gloss)
+
+        if e == 1 or e % 5 == 0:
+            plotGeneratedImages(e)
+            saveModels(e)
+
+    # Plot losses from every epoch
+    plotLoss(e)
+
+
+# In[12]:
+
+
+train(50, 128)
+
+
+# In[ ]:
+
+
+for _ in tqdm(range(50)):
+    print()
+
+
+# In[ ]:
+
+
+
+
